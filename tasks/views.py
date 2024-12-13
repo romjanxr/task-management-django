9319763da30c82266997b7b22d7b2bb896ac4dc1
Romjan Ali <romjanvr5@gmail.com>
2024-12-13 21:32:28 +0600

    Dynamic url implemented

--- tasks/views.py
+++ tasks/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return HttpResponse("Welcome to the task management system")
+
 
 def contact(request):
     return HttpResponse("<h1 style='color: red'>This is contact page</h1>")
 
+
 def show_task(request):
     return HttpResponse("This is our task page")
+
+
+def show_specific_task(request, id):
+    print("id", id)
+    print("id type", type(id))
+    return HttpResponse(f"This is specific task page {id} ")
